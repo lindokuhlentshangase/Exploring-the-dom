a8589dc3f758128f4c44b71ee87bea2acf7ba37b
Mbali <mbalimaduna76@gmail.com>
2024-07-04 20:06:14 +0200

    changes

---
+++ index.js
@@ -0,0 +1,36 @@
+const myMessage = document.querySelector('.myMessage');
+const theMessageButton = document.querySelector('.theMessageButton');
+const clearMessageButton = document.querySelector('.clearMessageButton');
+const inputBox = document.querySelector('.theInputValue');
+const fruitList = document.querySelector(".fruits");
+const fruitInput = document.querySelector('.inputFruit');
+const addFruitButton = document.querySelector('.addFruitButton')
+
+theMessageButton.addEventListener('click', function() {
+    myMessage.innerText = inputBox.value;
+});
+
+clearMessageButton.addEventListener('click', function() {
+    myMessage.innerText = '';
+});
+
+
+myMessage.addEventListener('click', function() {
+    myMessage.classList.toggle('darkmode')
+})
+
+
+const fruits = ['Apples', 'Pears', 'Oranges', 'Grapes', 'Bananas'];
+
+for(let i=0;i<fruits.length;i++){
+    const fruit = fruits[i];
+    const li = document.createElement('li');
+   li.innerText = fruit;
+   fruitList.appendChild(li);
+}
+
+addFruitButton.addEventListener('click', function() {
+    const li = document.createElement("li");
+    li.innerText = fruitInput.value;
+    fruitList.appendChild(li);
+})
